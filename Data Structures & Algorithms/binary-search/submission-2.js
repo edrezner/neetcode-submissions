8ee9a437736efb7cc3457b486b6d15bd0de79ffc
Eric Drezner <118303223+edrezner@users.noreply.github.com>
2026-07-27 14:35:48 -0400

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let start = 0;
+        let end = nums.length - 1;
+
+        while (start <= end) {
+            let mid = Math.floor((start + end) / 2);
+
+            if (target === nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+
+    }
+}
